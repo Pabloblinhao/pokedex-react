b1a2e996aaca416694beac2e6b76515d6772b6ed
pablo <pablo_vitor1707@hotmail.com>
2023-06-03 18:09:49 -0300

    implementando o load more

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -4,8 +4,9 @@ import PokemonCard from './PokemonCard';
 import './Pokedex.css';
 
 function Pokedex() {
-    const [pokemon, setPokemon] = useState([]); 
-    const [page, setPage] = useState(0);  // adicionado um estado para rastrear a página atual
+    const [pokemon, setPokemon] = useState([]);
+    const [visiblePokemonCount, setVisiblePokemonCount] = useState(18);
+    const [showLoadMore, setShowLoadMore] = useState(true);
 
     useEffect(() => {
         axios
@@ -17,32 +18,27 @@ function Pokedex() {
                 Promise.all(promises).then(pokemonResponses => {
                     const pokemonData = pokemonResponses.map(response => response.data);
                     setPokemon(pokemonData);
-                    console.log(pokemonData)
                 });
             });
     }, []);
 
-    const pokemonPerPage = 18;
-    const maxPage = Math.ceil(pokemon.length / pokemonPerPage);
-
-    const handleNext = () => {
-        setPage(prevPage => Math.min(prevPage + 1, maxPage - 1)); // Não vai além da última página
+    const loadMorePokemon = () => {
+        setVisiblePokemonCount(pokemon.length);
+        setShowLoadMore(false);
     };
-
-    const handlePrev = () => {
-        setPage(prevPage => Math.max(prevPage - 1, 0)); // Não vai antes da primeira página
-    };
-
-    // Seleciona somente os Pokémon para a página atual
-    const currentPokemon = pokemon.slice(page * pokemonPerPage, (page + 1) * pokemonPerPage);
 
     return (
         <div className="pokedex-grid">
-            {currentPokemon.map((p, index) => (  
-                <PokemonCard key={index} pokemon={p} pokemonNumber={index + 1}/> 
+            {pokemon.slice(0, visiblePokemonCount).map((p, index) => (
+                <PokemonCard key={index} pokemon={p} pokemonNumber={index + 1} />
             ))}
-            <button onClick={handlePrev} disabled={page === 0}>Previous</button>
-            <button onClick={handleNext} disabled={page === maxPage - 1}>Next</button>
+            {showLoadMore && (
+                <div className="load-more-container">
+                    <button className="load-more-button" onClick={loadMorePokemon}>
+                        Load More
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
